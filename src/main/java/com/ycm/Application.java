b1ee016ae892c72a37ba5a3cdecac048548e306b
Yang ChenMing <yang.chenming@oaknorth.ai>
2019-04-12 09:05:03 +0800

    cosmetic change

--- src/main/java/com/ycm/Application.java
+++ src/main/java/com/ycm/Application.java
@@ -1,5 +1,7 @@
 package com.ycm;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-
         //a tomcat server will be listening on port 8080
-
         SpringApplication.run(Application.class,args);
     }
 }
